e7a62fd2778bbc16a163facd82c7033365807b97
Richard S <rscheiwe@gmail.com>
2018-03-18 04:33:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyAppendKitten(name) {
-  kittens.append(name)
+  appendKitten(name)
   return kittens
 }
